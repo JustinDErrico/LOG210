9829fdf29cf2700ae0e33b81f15f685c3a82eee9
Justin D'Errico <just_skateforlife@hotmail.com>
2014-07-23 22:43:53 -0400

    Valid. resto et session
    -Les champs du restaurants sont validés
    -Si on est connecté et qu'on se connecte au root, au lieu de la page
    d'accueuil avec le user passeword, ca redirige au compte connecté

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,11 @@ class Restaurant < ActiveRecord::Base
   attr_accessible :address, :name, :linkedRestaurator, :linkedEntrepreneur
   attr_accessor :linkedRestaurator, :linkedEntrepreneur
 
+  #Validation des champs du formulaire
+  validates :name, length: { minimum: 2 }
+  validates :address, length: { minimum: 2 }
+  #Fin validation du formulaire
+
   has_many :menus
   belongs_to :restaurator
   belongs_to :entrepreneur
