c827868f89605557e1daae2879dab03b23a9f3fd
Vincent Dupuis <vincent.dupuis.lemelin@gmail.com>
2014-07-30 11:09:08 -0400

    fix tableau commandes

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -125,13 +125,4 @@ class MenusController < ApplicationController
       format.json { head :no_content }
     end
   end
-
- def nested_hash_value(obj,key)
-  if obj.respond_to?(:key?) && obj.key?(key)
-    obj[key]
-  elsif obj.respond_to?(:each)
-    r = nil
-    obj.find{ |*a| r=nested_hash_value(a.last,key) }
-    r
-  end
 end
